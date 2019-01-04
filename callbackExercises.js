b333c5ff8938ae434d59cc5bc8367bb17caa2a94
John <argote.john@gmail.com>
2019-01-03 22:34:54 -0600

    callback exercise complete

---
+++ callbackExercises.js
@@ -0,0 +1,124 @@
+//add callback function
+function add(x, y, resultFunction){
+    var result = x + y;
+    resultFunction(result);
+}
+
+function resultfun(result){
+    console.log(result);
+
+}
+
+add(1, 2, resultfun)
+
+
+//subraction call back function
+function subtract(x, y, resultFunc){
+    var result = x - y;
+    resultFunc(result);
+}
+
+function result(result){
+    console.log(result);
+}
+
+subtract(3, 1, result)
+
+
+//greeting call back function
+function greeting(person, greetFunc){
+    var result = `Hola, ${person}!`;
+    greetFunc(result);
+}
+
+function greetFunc(result){
+    console.log(result);
+}
+
+greeting('John', greetFunc)
+
+
+//multiplication callback function
+var num = [2, 3];
+
+function product(numbers, resultFunc){
+    var result = numbers.reduce(function(a,b){
+        return a * b;
+    }, 1);
+    resultFunc(result);
+}
+
+function resultFunc(result){
+    console.log(result);
+}
+
+product(num, resultFunc)
+
+
+
+
+
+//callback functions with delay of 1000 milliseconds
+//add callback function
+function add(x, y, resultFunction){
+    setTimeout(function(){
+        var result = x + y;
+        resultFunction(result);
+    }, 1000);
+}
+
+function resultfun(result){
+    console.log(result);
+
+}
+
+add(1, 2, resultfun)
+
+
+//subraction call back function
+function subtract(x, y, resultFunc){
+    setTimeout(()=>{
+        var result = x - y;
+        resultFunc(result);
+    },1000);
+}
+
+function result(result){
+    console.log(result);
+}
+
+subtract(3, 1, result)
+
+
+//greeting call back function
+function greeting(person, greetFunc){
+    setTimeout(()=>{
+        var result = `Hola, ${person}!`;
+        greetFunc(result);
+    }, 1000);
+}
+
+function greetFunc(result){
+    console.log(result);
+}
+
+greeting('John', greetFunc)
+
+
+//multiplication callback function
+var num = [2, 3];
+
+function product(numbers, resultFunc){
+    setTimeout(()=>{
+        var result = numbers.reduce(function(a,b){
+            return a * b;
+        }, 1);
+        resultFunc(result);
+    }, 1000);
+};
+
+function resultFunc(result){
+    console.log(result);
+};
+
+product(num, resultFunc)
